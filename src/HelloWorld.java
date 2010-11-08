bf0dfb32c2a89af001b379557c4f6a385168422c
Rodolfo de Lima Viana <rodolfolv@barbado.(none)>
2010-11-08 09:28:15 -0300

    ok !

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello World, everybody!\nTarciso");
+		System.out.println("Hello World, everybody!\nRodolfo");
 	}
 }
